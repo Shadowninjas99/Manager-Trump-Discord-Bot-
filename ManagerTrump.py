b8d6e4f15c84f041b11dc338fac9f7968d2c3abf
Anirban Dey <77397521+Shadowninjas99@users.noreply.github.com>
2021-03-17 11:25:24 +0530

    Create ManagerTrump.py

---
+++ ManagerTrump.py
@@ -0,0 +1,615 @@
+import discord
+from discord.ext import commands, tasks
+import asyncio
+from itertools import cycle
+import os
+import json
+import random
+import giphy_client
+from giphy_client.rest import ApiException
+import os
+from keep_alive import keep_alive
+
+client = commands.Bot(command_prefix='$')
+
+client.remove_command("help")
+
+status = cycle(
+    ['Try $help','Prefix -$'])
+
+
+@client.event
+async def on_ready():
+    change_status.start()
+    print('Manager Trump is ready')
+
+@tasks.loop(seconds=5)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
+
+
+####################################################################
+####################################################################
+# Main code starts :)
+
+mainshop = [{"name":"Watch","price":100,"description":"⌚"},
+            {"name":"Mobile","price":1000,"description":"📱"},
+            {"name":"Dog","price":200,"description":"🐕 "},
+            {"name":"Laptop","price":4000,"description":"💻"},
+            {"name":"PC","price":17000,"description":"🖥️"},
+            {"name":"Car","price":40000,"description":"🚗"},
+            {"name":"Diamond","price":85000,"description":"💎"},
+            {"name":"Crown","price":150000,"description":"👑"},
+            {"name":"Gucci","price":20000,"description":"👕"},
+            {"name":"LV Bags","price":22000,"description":"👜"},
+            {"name":"Business","price":2000000,"description":"🏢"},
+            {"name":"House","price":800000,"description":"🏠"},
+            {"name":"Tiger","price":80000,"description":"🐯"},
+            {"name":"Love","price":800000,"description":"❤️"},
+            {"name":"Ferrari","price":299999,"description":"🏎️"},
+            {"name":"Bank","price":2000000,"description":"🏛️"}]
+
+@client.command(aliases=['bal'])
+async def balance(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+
+    users = await get_bank_data()
+
+    wallet_amt = users[str(user.id)]["wallet"]
+    bank_amt = users[str(user.id)]["bank"]
+
+    em = discord.Embed(title=f'{ctx.author.name} Balance',color = discord.Color.blue())
+    em.add_field(name="Wallet Balance", value=wallet_amt)
+    em.add_field(name='Bank Balance',value=bank_amt)
+    await ctx.send(embed= em)
+
+@client.command()
+async def beg(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+
+    users = await get_bank_data()
+
+    earnings = random.randrange(101)
+
+    await ctx.send(f'{ctx.author.mention} Got {earnings} 💵 !!')
+
+    users[str(user.id)]["wallet"] += earnings
+
+    with open("mainbank.json",'w') as f:
+        json.dump(users,f)
+
+
+@client.command(aliases=['wd'])
+async def withdraw(ctx,amount = None):
+    await open_account(ctx.author)
+    if amount == None:
+        await ctx.send("Please enter the amount")
+        return
+
+    bal = await update_bank(ctx.author)
+
+    amount = int(amount)
+
+    if amount > bal[1]:
+        await ctx.send('You do not have sufficient balance')
+        return
+    if amount < 0:
+        await ctx.send('Amount must be positive!')
+        return
+
+    await update_bank(ctx.author,amount)
+    await update_bank(ctx.author,-1*amount,'bank')
+    await ctx.send(f'{ctx.author.mention} You withdrew {amount} 💵')
+
+
+@client.command(aliases=['dp'])
+async def deposit(ctx,amount = None):
+    await open_account(ctx.author)
+    if amount == None:
+        await ctx.send("Please enter the amount")
+        return
+
+    bal = await update_bank(ctx.author)
+
+    amount = int(amount)
+
+    if amount > bal[0]:
+        await ctx.send('You do not have sufficient balance')
+        return
+    if amount < 0:
+        await ctx.send('Amount must be positive!')
+        return
+
+    await update_bank(ctx.author,-1*amount)
+    await update_bank(ctx.author,amount,'bank')
+    await ctx.send(f'{ctx.author.mention} You deposited {amount} 💵')
+
+
+@client.command(aliases=['sm'])
+async def send(ctx,member : discord.Member,amount = None):
+    await open_account(ctx.author)
+    await open_account(member)
+    if amount == None:
+        await ctx.send("Please enter the amount")
+        return
+
+    bal = await update_bank(ctx.author)
+    if amount == 'all':
+        amount = bal[0]
+
+    amount = int(amount)
+
+    if amount > bal[0]:
+        await ctx.send('You do not have sufficient balance')
+        return
+    if amount < 0:
+        await ctx.send('Amount must be positive!')
+        return
+
+    await update_bank(ctx.author,-1*amount,'bank')
+    await update_bank(member,amount,'bank')
+    await ctx.send(f'{ctx.author.mention} You gave {member} {amount} 💸')
+
+
+@client.command(aliases=['rb'])
+async def rob(ctx,member : discord.Member):
+    await open_account(ctx.author)
+    await open_account(member)
+    bal = await update_bank(member)
+
+
+    if bal[0]<100:
+        await ctx.send('It is useless to rob him :(')
+        return
+
+    earning = random.randrange(0,bal[0])
+
+    await update_bank(ctx.author,earning)
+    await update_bank(member,-1*earning)
+    await ctx.send(f'{ctx.author.mention} You robbed {member} and got {earning} 💸')
+
+
+@client.command()
+async def slots(ctx,amount = None):
+    await open_account(ctx.author)
+    if amount == None:
+        await ctx.send("Please enter the amount")
+        return
+
+    bal = await update_bank(ctx.author)
+
+    amount = int(amount)
+
+    if amount > bal[0]:
+        await ctx.send('You do not have sufficient balance')
+        return
+    if amount < 0:
+        await ctx.send('Amount must be positive!')
+        return
+    final = []
+    for i in range(3):
+        a = random.choice(['X','O','Q'])
+
+        final.append(a)
+
+    await ctx.send(str(final))
+
+    if final[0] == final[1] or final[1] == final[2] or final[0] == final[2]:
+        await update_bank(ctx.author,2*amount)
+        await ctx.send(f'You won 🥳 {ctx.author.mention}')
+    else:
+        await update_bank(ctx.author,-1*amount)
+        await ctx.send(f'You lose 😭 {ctx.author.mention}')
+
+
+@client.command()
+async def shop(ctx):
+    em = discord.Embed(title = "Shop")
+
+    for item in mainshop:
+        name = item["name"]
+        price = item["price"]
+        desc = item["description"]
+        em.add_field(name = name, value = f"${price} | {desc}")
+
+    await ctx.send(embed = em)
+
+
+
+@client.command()
+async def buy(ctx,item,amount = 1):
+    await open_account(ctx.author)
+
+    res = await buy_this(ctx.author,item,amount)
+
+    if not res[0]:
+        if res[1]==1:
+            await ctx.send("That Object isn't there!")
+            return
+        if res[1]==2:
+            await ctx.send(f"You don't have enough money in your wallet to buy {amount} {item}")
+            return
+
+
+    await ctx.send(f"You just bought {amount} {item}")
+
+
+@client.command()
+async def bag(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await get_bank_data()
+
+    try:
+        bag = users[str(user.id)]["bag"]
+    except:
+        bag = []
+
+
+    em = discord.Embed(title = "Bag")
+    for item in bag:
+        name = item["item"]
+        amount = item["amount"]
+
+        em.add_field(name = name, value = amount)    
+
+    await ctx.send(embed = em)
+
+
+async def buy_this(user,item_name,amount):
+    item_name = item_name.lower()
+    name_ = None
+    for item in mainshop:
+        name = item["name"].lower()
+        if name == item_name:
+            name_ = name
+            price = item["price"]
+            break
+
+    if name_ == None:
+        return [False,1]
+
+    cost = price*amount
+
+    users = await get_bank_data()
+
+    bal = await update_bank(user)
+
+    if bal[0]<cost:
+        return [False,2]
+
+
+    try:
+        index = 0
+        t = None
+        for thing in users[str(user.id)]["bag"]:
+            n = thing["item"]
+            if n == item_name:
+                old_amt = thing["amount"]
+                new_amt = old_amt + amount
+                users[str(user.id)]["bag"][index]["amount"] = new_amt
+                t = 1
+                break
+            index+=1 
+        if t == None:
+            obj = {"item":item_name , "amount" : amount}
+            users[str(user.id)]["bag"].append(obj)
+    except:
+        obj = {"item":item_name , "amount" : amount}
+        users[str(user.id)]["bag"] = [obj]        
+
+    with open("mainbank.json","w") as f:
+        json.dump(users,f)
+
+    await update_bank(user,cost*-1,"wallet")
+
+    return [True,"Worked"]
+    
+
+@client.command()
+async def sell(ctx,item,amount = 1):
+    await open_account(ctx.author)
+
+    res = await sell_this(ctx.author,item,amount)
+
+    if not res[0]:
+        if res[1]==1:
+            await ctx.send("That Object isn't there!")
+            return
+        if res[1]==2:
+            await ctx.send(f"You don't have {amount} {item} in your bag.")
+            return
+        if res[1]==3:
+            await ctx.send(f"You don't have {item} in your bag.")
+            return
+
+    await ctx.send(f"You just sold {amount} {item}.")
+
+async def sell_this(user,item_name,amount,price = None):
+    item_name = item_name.lower()
+    name_ = None
+    for item in mainshop:
+        name = item["name"].lower()
+        if name == item_name:
+            name_ = name
+            if price==None:
+                price = 0.7* item["price"]
+            break
+
+    if name_ == None:
+        return [False,1]
+
+    cost = price*amount
+
+    users = await get_bank_data()
+
+    bal = await update_bank(user)
+
+
+    try:
+        index = 0
+        t = None
+        for thing in users[str(user.id)]["bag"]:
+            n = thing["item"]
+            if n == item_name:
+                old_amt = thing["amount"]
+                new_amt = old_amt - amount
+                if new_amt < 0:
+                    return [False,2]
+                users[str(user.id)]["bag"][index]["amount"] = new_amt
+                t = 1
+                break
+            index+=1 
+        if t == None:
+            return [False,3]
+    except:
+        return [False,3]    
+
+    with open("mainbank.json","w") as f:
+        json.dump(users,f)
+
+    await update_bank(user,cost,"wallet")
+
+    return [True,"Worked"]
+
+
+@client.command(aliases = ["lb"])
+async def leaderboard(ctx,x = 1):
+    users = await get_bank_data()
+    leader_board = {}
+    total = []
+    for user in users:
+        name = int(user)
+        total_amount = users[user]["wallet"] + users[user]["bank"]
+        leader_board[total_amount] = name
+        total.append(total_amount)
+
+    total = sorted(total,reverse=True)    
+
+    em = discord.Embed(title = f"Top {x} Richest People" , description = "This is decided on the basis of raw money in the bank and wallet",color = discord.Color(0xfa43ee))
+    index = 1
+    for amt in total:
+        id_ = leader_board[amt]
+        member = client.get_user(id_)
+        name = member.name
+        em.add_field(name = f"{index}. {name}" , value = f"{amt}",  inline = False)
+        if index == x:
+            break
+        else:
+            index += 1
+
+    await ctx.send(embed = em)
+
+
+async def open_account(user):
+
+    users = await get_bank_data()
+
+    if str(user.id) in users:
+        return False
+    else:
+        users[str(user.id)] = {}
+        users[str(user.id)]["wallet"] = 0
+        users[str(user.id)]["bank"] = 0
+
+    with open('mainbank.json','w') as f:
+        json.dump(users,f)
+
+    return True
+
+
+async def get_bank_data():
+    with open('mainbank.json','r') as f:
+        users = json.load(f)
+
+    return users
+
+
+async def update_bank(user,change=0,mode = 'wallet'):
+    users = await get_bank_data()
+
+    users[str(user.id)][mode] += change
+
+    with open('mainbank.json','w') as f:
+        json.dump(users,f)
+    bal = users[str(user.id)]['wallet'],users[str(user.id)]['bank']
+    return bal
+
+
+
+
+# Main code ends :(
+####################################################################
+####################################################################
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+@client.command()
+async def gif(ctx,*,q="Random"):
+
+    api_key= 'n1g1DGOK4qJKqJykePdkP1hZhz40TwLr'
+    api_instance= giphy_client.DefaultApi()
+
+    try:
+
+      api_response= api_instance.gifs_search_get(api_key, q, limit=5, rating='g')
+      lst=(api_response.data)
+      giff = random.choice(lst)
+      
+      await ctx.channel.send(giff.embed_url)
+
+    except ApiException as e:
+      print("Exception when calling Api")
+
+
+@client.command()
+async def poll(ctx,*,message):
+  emb=discord.Embed(title=" POLL", description=f"{message}")
+  msg=await ctx.channel.send(embed=emb)
+  await msg.add_reaction('👍')
+  await msg.add_reaction('👎')
+
+  
+
+player1 = ""
+player2 = ""
+turn = ""
+gameOver = True
+
+board = []
+
+winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+@client.command()
+async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+    global count
+    global player1
+    global player2
+    global turn
+    global gameOver
+
+    if gameOver:
+        global board
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        turn = ""
+        gameOver = False
+        count = 0
+
+        player1 = p1
+        player2 = p2
+
+        # print the board
+        line = ""
+        for x in range(len(board)):
+            if x == 2 or x == 5 or x == 8:
+                line += " " + board[x]
+                await ctx.send(line)
+                line = ""
+            else:
+                line += " " + board[x]
+
+        # determine who goes first
+        num = random.randint(1, 2)
+        if num == 1:
+            turn = player1
+            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
+        elif num == 2:
+            turn = player2
+            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
+    else:
+        await ctx.send("A game is already in progress! Finish it before starting a new one.")
+
+@client.command()
+async def place(ctx, pos: int):
+    global turn
+    global player1
+    global player2
+    global board
+    global count
+    global gameOver
+
+    if not gameOver:
+        mark = ""
+        if turn == ctx.author:
+            if turn == player1:
+                mark = ":regional_indicator_x:"
+            elif turn == player2:
+                mark = ":o2:"
+            if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
+                board[pos - 1] = mark
+                count += 1
+
+                # print the board
+                line = ""
+                for x in range(len(board)):
+                    if x == 2 or x == 5 or x == 8:
+                        line += " " + board[x]
+                        await ctx.send(line)
+                        line = ""
+                    else:
+                        line += " " + board[x]
+
+                checkWinner(winningConditions, mark)
+                print(count)
+                if gameOver == True:
+                    await ctx.send(mark + " wins!")
+                elif count >= 9:
+                    gameOver = True
+                    await ctx.send("It's a tie!")
+
+                # switch turns
+                if turn == player1:
+                    turn = player2
+                elif turn == player2:
+                    turn = player1
+            else:
+                await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
+        else:
+            await ctx.send("It is not your turn.")
+    else:
+        await ctx.send("Please start a new game using the !tictactoe command.")
+
+
+def checkWinner(winningConditions, mark):
+    global gameOver
+    for condition in winningConditions:
+        if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
+            gameOver = True
+
+@tictactoe.error
+async def tictactoe_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please mention 2 players for this command.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to mention/ping players (ie. <@688534433879556134>).")
+
+@place.error
+async def place_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please enter a position you would like to mark.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to enter an integer.")
+
+
+
+
+keep_alive()
+client.run(os.getenv('Token'))
